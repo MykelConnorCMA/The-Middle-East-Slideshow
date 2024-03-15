9807e818c0dea56918a81703e24a4ee20519f344
MykelConnorCMA <163432407+MykelConnorCMA@users.noreply.github.com>
2024-03-15 13:44:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const slide = document.getElementById("slide");
-const img = new Image();
+const img = document.getElementById("image");
 
-const slide_1 = "/slides/1.png";
+const slide_1 = "https://github.com/MykelConnorCMA/The-Middle-East-Slideshow/blob/43583f687d09e77c47fc0876189d19bf7c863d7c/slides/1.png";
+const slide_2 = "https://github.com/MykelConnorCMA/The-Middle-East-Slideshow/blob/43583f687d09e77c47fc0876189d19bf7c863d7c/slides/2.png";
+const slide_3 = "https://github.com/MykelConnorCMA/The-Middle-East-Slideshow/blob/43583f687d09e77c47fc0876189d19bf7c863d7c/slides/3.png";
+const slide_4 = "https://github.com/MykelConnorCMA/The-Middle-East-Slideshow/blob/43583f687d09e77c47fc0876189d19bf7c863d7c/slides/4.png";
