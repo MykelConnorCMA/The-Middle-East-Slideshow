dba45565dfea85d0c98879a9bb1948ed85e91224
MykelConnorCMA <163432407+MykelConnorCMA@users.noreply.github.com>
2024-03-15 14:37:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const img = document.getElementById("image");
 
-const slide_1 = "https://github.com/MykelConnorCMA/The-Middle-East-Slideshow/blob/43583f687d09e77c47fc0876189d19bf7c863d7c/slides/1.png";
-const slide_2 = "https://github.com/MykelConnorCMA/The-Middle-East-Slideshow/blob/43583f687d09e77c47fc0876189d19bf7c863d7c/slides/2.png";
-const slide_3 = "https://github.com/MykelConnorCMA/The-Middle-East-Slideshow/blob/43583f687d09e77c47fc0876189d19bf7c863d7c/slides/3.png";
-const slide_4 = "https://github.com/MykelConnorCMA/The-Middle-East-Slideshow/blob/43583f687d09e77c47fc0876189d19bf7c863d7c/slides/4.png";
+const slide_1 = new Image();
+slide_1.src = "https://github.com/MykelConnorCMA/The-Middle-East-Slideshow/blob/43583f687d09e77c47fc0876189d19bf7c863d7c/slides/1.png";
+const slide_2 = new Image();
+slide_2.src = "https://github.com/MykelConnorCMA/The-Middle-East-Slideshow/blob/43583f687d09e77c47fc0876189d19bf7c863d7c/slides/2.png";
+const slide_3 = new Image();
+slide_3.src = "https://github.com/MykelConnorCMA/The-Middle-East-Slideshow/blob/43583f687d09e77c47fc0876189d19bf7c863d7c/slides/3.png";
+const slide_4 = new Image();
+slide_4.src = "https://github.com/MykelConnorCMA/The-Middle-East-Slideshow/blob/43583f687d09e77c47fc0876189d19bf7c863d7c/slides/1.png";
